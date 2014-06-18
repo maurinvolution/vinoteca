c2c054123023e104d8362c5c74bddd9eec482b06
maurinvolution <maurig_666@hotmail.com>
2014-06-18 11:42:17 -0300

    Update producto.php

--- modulos/producto.php
+++ modulos/producto.php
@@ -1,6 +1,10 @@
 <section>
     <div id="producto">
-        <img src="#">
-        <p></p>
+        <img src="">
+        <h1>nombre de prod</h1>
+        <h3>precio</h3>
+        <p>stock</p>
+        <p>caracteristicas</p>
+        <p>descripcion</p>
     </div>
 </section>
