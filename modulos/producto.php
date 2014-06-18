3fe481ce08f0f676e6ec6809ca081715abf7a2d0
maurinvolution <maurig_666@hotmail.com>
2014-06-18 12:00:22 -0300

    Update producto.php

--- modulos/producto.php
+++ modulos/producto.php
@@ -2,11 +2,11 @@
     <div id="producto">
     	<?php
         echo "<img src='$resu_sql[img]'>";
-        echo "<h1>$resu_sql[marca]</h1>"; <!-- nombre de prod -->
-        echo "<h3>$resu_sql[precio]</h3>";	<!-- precio -->
-        echo "<p>$resu_sql[stock]</p>";	<!-- stock -->
-        echo "<p>$resu_sql[caracteristica]</p>";	<!-- caracteristicas -->
-        echo "<p>$resu_sql[descripcion]</p>"; 	<!-- descripcion -->
+        echo "<h1>$resu_sql[marca]</h1>";   //<!-- nombre de prod -->
+        echo "<h3>$resu_sql[precio]</h3>";	// <!-- precio -->
+        echo "<p>$resu_sql[stock]</p>";	    //<!-- stock -->
+        echo "<p>$resu_sql[caracteristica]</p>";	//<!-- caracteristicas -->
+        echo "<p>$resu_sql[descripcion]</p>"; 	//<!-- descripcion -->
         ?>
     </div>
 </section>
