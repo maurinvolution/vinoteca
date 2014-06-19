b22a6be83c40fd151477ef2bac2c58503d27fb5b
maurinvolution <maurig_666@hotmail.com>
2014-06-19 15:38:36 -0300

    Update producto.php

--- modulos/producto.php
+++ modulos/producto.php
@@ -2,14 +2,14 @@
     <div id="producto">
     	<?php
     	echo '<div id="img_prod">';
-        echo "<img src='$resu_sql[img]'>";
+        echo '<img src="'.$resu_sql[img].'">';
         echo '</div>';
-        <div id="datos_prod">
-        echo "<h1>$resu_sql[marca]</h1>";   //<!-- nombre de prod -->
-        echo "<h3>$resu_sql[precio]</h3>";	// <!-- precio -->
-        echo "<p>$resu_sql[stock]</p>";	    //<!-- stock -->
-        echo "<p>$resu_sql[caracteristica]</p>";	//<!-- caracteristicas -->
-        echo "<p>$resu_sql[descripcion]</p>"; 	//<!-- descripcion -->
+        echo '<div id="datos_prod">';
+        echo '<h1>'.$resu_sql[marca].'</h1>';   //<!-- nombre de prod -->
+        echo '<h3>'.$resu_sql[precio].'</h3>';	// <!-- precio -->
+        echo '<p>'.$resu_sql[stock].'</p>';	    //<!-- stock -->
+        echo '<p>'.$resu_sql[caracteristica].'</p>';	//<!-- caracteristicas -->
+        echo '<p>'.$resu_sql[descripcion].'</p>'; 	//<!-- descripcion -->
         echo '</div>';
         ?>
     </div>
