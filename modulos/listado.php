096bcd227ed97094b4fcda0cc964a0d99f346363
maurinvolution <maurig_666@hotmail.com>
2014-06-26 23:37:25 -0300

    Update listado.php

--- modulos/listado.php
+++ modulos/listado.php
@@ -1,66 +1,64 @@
-<header id="listado">
-    <nav id="nav">
-    <ul>
-        <li><a href="#">Vinos</a></li>
-        <li><a href="#">Whiskies</a></li>
-        <li><a href="#">Champagne</a></li>
-        <li><a href="#">Regalos empresariales</a></li>
-        <li><a href="#">Bebidas de alto grado</a></li>
-        <li><a href="#">Accesorios</a></li>
-        <li><a href="#">Buscar</a></li>
-        <li><a href="#">Promocioness</a></li>
-        
-    </ul>
-    </nav>
-</header>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Su Bodega</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<aside>
-<nav id="filtros">
-    <ul>
-    <li class="oculto">Color 
-        <ul class="sombra">
-            <li><a href="#">Tintos</a></li>
-            <li><a href="#">Blancos</a></li>
-            <li><a href="#">Rosé</a></li>
-            <li><a href="#">más</a></li>
-        </ul>
-    </li>
-    <li class="oculto">Cepa 
-        <ul class="sombra">
-            <?php
-            while ( $resu_cepa = @mysql_fetch_array($sql_cepa) ) 
-            {
-                echo '<li><a href="#">'.$resu_cepa[descripcion].'</a></li>';
-            }
-            ?>
-            <li><a href="#">más</a></li>
-        </ul>
-    </li>
-    <li class="oculto">Bodega 
-        <ul class="sombra">
-            <?php
-            while ( $resu_bodega = @mysql_fetch_array($sql_bodega) ) 
-            {
-                echo '<li><a href="#">'.$resu_bodega[descripcion].'</a></li>';
-            }
-            ?>
-            <li><a href="#">más</a></li>
-        </ul>
-    </li>
-    </ul>
-</nav>
-</aside>
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-<section>
-    <div id="producto">
-     <?php
-        while ( $resu_prod = @mysql_fetch_array($sql_cepa) ) 
-        {
-            echo "<img src='$resu_sql[img]'>";
-            echo "<h1>$resu_prod[marca]</h1>"; //<!-- nombre de prod -->
-            echo "<h3>$resu_prod[precio]</h3>";  // <!-- precio -->
-            echo "<p>$resu_prod[stock]</p>"; //<!-- stock -->
-            echo "<p>$resu_prod[caracteristica]</p>";    //<!-- caracteristicas -->
-        }?>
-    </div>
-</section>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+<div id="contenedor">
+        <table align="center">
+<tr>
+    <td><a href="home.php?pagina=listado&var=vinos"><img src="/img/vinos.jpg"></a></td>
+    <td><a href="home.php?pagina=listado&var=regalos"><img src="/img/regalos.jpg"></a></td>
+    <td><a href=""><img src="/img/buscar.jpg"></a></td>
+</tr>
+<tr>
+    <td><a href="home.php?pagina=listado&var=whiskies"><img src="/img/whiskies.jpg"></a></td>
+    <td><a href="home.php?pagina=listado&var=bebidas"><img src="/img/bebidas.jpg"></a></td>
+    <td rowspan="2"><a href="home.php?pagina=listado&var=promo"><img src="/img/promo.jpg"></a></td>
+</tr>
+<tr>
+    <td><a href="home.php?pagina=listado&var=champagne"><img src="/img/champagne.jpg"></a></td>
+    <td><a href="home.php?pagina=listado&var=accesorios"><img src="/img/accesorios.jpg"></a></td>
+</tr>
+        </table>
+        <div id="foot">   
+             <img src="/img/foot.jpg">
+</div>
+
+</div> <!-- fin contenedor -->
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
